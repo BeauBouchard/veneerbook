767e746b92bac72a3371d4e4417c249f427cf5f6
beaubouchard <beau@beaubouchard.com>
2019-11-01 17:57:15 -0400

    feat: hello world, with a /health endpoint.

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import logger from 'winston';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+const pkjson = require('../package.json');
+
+const app = express();
+
+// adding helmet
+app.use(helmet());
+
+// using bodyParser to parse json bodies into js objects
+app.use(bodyParser.json());
+
+/** set up cors middleware
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.use((req, res, next) => {
+  // NOTE: this is very open and generous for demonstration purous
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, Origin, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+
+logger.info('turning on app...');
+
+/**
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {Next} next - Express Next object
+ */
+app.get('/health', (req, res, next) => {
+  const time = process.uptime();
+  res.status(200).send({ data: {uptime: time, version: pkjson.version} });
+});
+
+// heroku dynamically assigns your app a port,
+// so you can't set the port to a fixed number.
+const server = app.listen(process.env.PORT || 5000, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  logger.info(`api listening at http://${host}:${port}`);
+});
+
+module.exports = server;
